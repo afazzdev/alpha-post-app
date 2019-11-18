eb8067892d1f74f178da83816c71ed6043a192d3
Pondok Programmer <pondokit@Thinkpad-X200s>
2019-11-18 10:14:26 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from "react";
 import Routes from "./routes/routes";
 
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import { Reducers } from "./redux/reducers";
+
+const Store = createStore(
+  Reducers,
+  {},
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__
+      ? window.__REDUX_DEVTOOLS_EXTENSION__()
+      : f => f
+  )
+);
+
 function App() {
   return (
-    <React.Fragment>
-      <Routes />
-    </React.Fragment>
+    <div className="App">
+      <Provider store={Store}>
+        <Routes />
+      </Provider>
+    </div>
   );
 }
 
